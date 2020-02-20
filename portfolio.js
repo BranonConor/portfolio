0054a785566e82113fcc21e5707967acb1938af9
branonconor <breusebio@westmont.edu>
2020-02-19 17:56:25 -0800

    Changed entire navbar structure

--- portfolio.js
+++ portfolio.js
@@ -2,67 +2,41 @@ AOS.init();
 $(function() {
     $(window).scroll(function () {
        if ($(this).scrollTop() > 50) {
-          $('.navbar').addClass('changeColor')
+          $('.nav').addClass('changeColor')
        }
        if ($(this).scrollTop() < 50) {
-          $('.navbar').removeClass('changeColor')
+          $('.nav').removeClass('changeColor')
        }
     });
  });
-// --- Typing --- //
-var TxtRotate = function(el, toRotate, period) {
-   this.toRotate = toRotate;
-   this.el = el;
-   this.loopNum = 0;
-   this.period = parseInt(period, 10) || 2000;
-   this.txt = '';
-   this.tick();
-   this.isDeleting = false;
- };
- 
- TxtRotate.prototype.tick = function() {
-   var i = this.loopNum % this.toRotate.length;
-   var fullTxt = this.toRotate[i];
- 
-   if (this.isDeleting) {
-     this.txt = fullTxt.substring(0, this.txt.length - 1);
-   } else {
-     this.txt = fullTxt.substring(0, this.txt.length + 1);
-   }
- 
-   this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
- 
-   var that = this;
-   var delta = 300 - Math.random() * 100;
- 
-   if (this.isDeleting) { delta /= 2; }
- 
-   if (!this.isDeleting && this.txt === fullTxt) {
-     delta = this.period;
-     this.isDeleting = true;
-   } else if (this.isDeleting && this.txt === '') {
-     this.isDeleting = false;
-     this.loopNum++;
-     delta = 500;
-   }
- 
-   setTimeout(function() {
-     that.tick();
-   }, delta);
- };
- 
- window.onload = function() {
-   var elements = document.getElementsByClassName('txt-rotate');
-   for (var i=0; i<elements.length; i++) {
-     var toRotate = elements[i].getAttribute('data-rotate');
-     var period = elements[i].getAttribute('data-period');
-     if (toRotate) {
-       new TxtRotate(elements[i], JSON.parse(toRotate), period);
-     }
-   }
-   // INJECT CSS
-   var css = document.createElement("style");
-   css.type = "text/css";
-   css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
-   document.body.appendChild(css);
- };
+
+
+ // GLOBALS
+const hamburger = document.querySelector('.hamburger');
+const navMobile = document.querySelector('.nav-mobile');
+const mobileLinks = document.querySelectorAll('.mobileLink');
+const cards = document.querySelectorAll('.card');
+
+// HELPER FUNCS
+const expandMenu = () => {
+    // take the mobile nav menu and reveal it on a hamburger click
+    navMobile.classList.toggle('revealNavMobile');
+}
+
+
+// Burger animation
+const hamburgerClick = (x) => {
+    x.classList.toggle('change');
+}
+
+// Expand Mobile Menu
+hamburger.addEventListener('click', function() {
+    expandMenu();
+});
+
+// Expand cards
+cards.forEach((item) => {
+    item.addEventListener('click', () => {
+        item.classList.toggle('move');
+    });
+});
